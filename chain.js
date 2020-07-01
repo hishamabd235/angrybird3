fa7a4ec2393da7a619c3247fc85d217fb5f732ec
hishamabd235 <65910788+hishamabd235@users.noreply.github.com>
2020-07-01 19:43:43 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var options = {
+            bodyA:bodyA,
+            bodyB:bodyB,
+            length:10,
+            stiffness:0.1
+        }
+        this.chain=Matter.Constraint.create(options)
+        World.add(world,this.chain)
+    }
+    display(){
+        var a = this.chain.bodyA.position
+        var b = this.chain.bodyB.position
+        line(a.x,a.y,b.x,b.y)
+    }
+}
